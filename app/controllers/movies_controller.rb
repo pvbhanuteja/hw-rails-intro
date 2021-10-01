e412d541ceac6b708bd8f69004178e6d90d6a315
Ubuntu <ubuntu@ip-172-31-82-27.ec2.internal>
2021-10-01 00:51:05 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,24 +7,45 @@ class MoviesController < ApplicationController
     end
   
     def index
-      @movies = Movie.all
+      # @movies = Movie.all
       @all_ratings = Movie.ratings
-      @ratings_hash = @all_ratings.map {|key| [key, 1]}.to_h
-      sort = params[:sort]
+      # @ratings_hash = @all_ratings.map {|key| [key, 1]}.to_h
+      # sort = params[:sort]
+      # if (params[:ratings] != nil)
+      # @ratings_hash = params[:ratings]
+      # @movies = @movies.where(:rating => @ratings_hash.keys)
+      # session[:ratings] = @ratings_hash
+      # end
+      # # p params
+      # p @ratings_hash
+      # case sort
+      # when 'title'
+      #   @title_class = 'bg_highlight'
+      #   @movies = Movie.order('title ASC')
+      # when 'release_date'
+      #   @release_date_class =  'bg_highlight'
+      #   @movies = Movie.order('release_date ASC')
+      # end
       if (params[:ratings] != nil)
-      @ratings_hash = params[:ratings]
-      @movies = @movies.where(:rating => @ratings_hash.keys)
-      session[:ratings] = @ratings_hash
+        session[:ratings] = params[:ratings]
       end
-      # p params
-      p @ratings_hash
-      case sort
-      when 'title'
-        @title_class = 'bg_highlight'
-        @movies = Movie.order('title ASC')
-      when 'release_date'
-        @release_date_class =  'bg_highlight'
-        @movies = Movie.order('release_date ASC')
+      if (params[:sort] != nil)
+        session[:sort] = params[:sort]
+      end
+      
+      if (params[:ratings] == nil && session[:ratings] != nil) || (params[:sort] == nil && session[:sort] != nil)
+        redirect_to movies_path("sort" => session[:sort], "ratings" => session[:ratings])
+      elsif params[:ratings] != nil || params[:sort] != nil
+        if params[:ratings] == nil
+          return @movies = Movie.all.order(session[:sort])
+        else
+          array_ratings = params[:ratings].keys
+          return @movies = Movie.where(rating: array_ratings).order(session[:sort])
+        end
+      elsif session[:ratings] != nil || session[:sort] != nil
+        redirect_to movies_path("ratings" => session[:ratings], "sort" => session[:sort])
+      else
+        return @movies = Movie.all
       end
       
     end
@@ -36,6 +57,7 @@ class MoviesController < ApplicationController
     def create
       @movie = Movie.create!(movie_params)
       flash[:notice] = "#{@movie.title} was successfully created."
+      flash.keep
       redirect_to movies_path
     end
   
@@ -47,6 +69,7 @@ class MoviesController < ApplicationController
       @movie = Movie.find params[:id]
       @movie.update_attributes!(movie_params)
       flash[:notice] = "#{@movie.title} was successfully updated."
+      flash.keep
       redirect_to movie_path(@movie)
     end
   
@@ -54,6 +77,7 @@ class MoviesController < ApplicationController
       @movie = Movie.find(params[:id])
       @movie.destroy
       flash[:notice] = "Movie '#{@movie.title}' deleted."
+      flash.keep
       redirect_to movies_path
     end
   
--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,4 +3,25 @@ module MoviesHelper
   def oddness(count)
     count.odd? ?  "odd" :  "even"
   end
+  
+  def selected_ratings(rating)
+    if (session[:ratings] != nil)
+      if(session[:ratings].include? rating)
+        return true
+      else
+        return false
+      end
+    else
+      return false
+    end
+  end
+  def bg_highlight(sort_column)
+    if(session[:sort].to_s == sort_column)
+      return 'bg_highlight'
+    else
+      return ''
+    end
+  end
 end
+
+
